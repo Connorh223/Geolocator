e1d70951b7bf92b6c570109cbdba73781f0c0484
Connor Hickman <connorh223@gmail.com>
2022-03-19 00:30:57 -0400

    .

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from geopy.geocoders import Nominatim
 
-# generates lat and long for an address
+# generates lat and long for using an address input
 print("GEOLOCATING")
 geolocator = Nominatim(user_agent="Vade_test_question")
 location = geolocator.geocode("1128 Berwyn Way NC")
 print(location.address)
 print((location.latitude, location.longitude))
 
-# reverse generates an adddress from lat and long
+# reverse generates an adddress using lat and long input
 print("REVERSE GEOLOCATING")
 geolocator = Nominatim(user_agent="Vade_test_question")
 location = geolocator.reverse("35.91847454422203, -78.67605333997926")
